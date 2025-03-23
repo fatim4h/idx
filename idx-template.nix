97b7ce29f25126766dec5606a8f441ed685eb9f6
Fatimah <165118476+fatim4h@users.noreply.github.com>
2025-03-24 03:39:58 +0700

    a

--- idx-template.nix
+++ idx-template.nix
@@ -1,4 +1,4 @@
-{ pkgs, packageManager ? "npm", ... }: {
+{ pkgs, authToken ? "", ... }: {
   packages = [
     pkgs.nodejs_20
     pkgs.yarn
@@ -8,14 +8,10 @@
   ];
   bootstrap = ''
     mkdir -p "$WS_NAME"
-    
-    # Initialize the project using npx directly
-    npx -y @react-native-community/cli init "$WS_NAME" --skip-install
-    
     mkdir "$WS_NAME/.idx/"
-    packageManager=${packageManager} j2 ${./devNix.j2} -o "$WS_NAME/.idx/dev.nix"
+    authToken=${authToken} j2 ${./devNix.j2} -o "$WS_NAME/.idx/dev.nix"
     nixfmt "$WS_NAME/.idx/dev.nix"
-    packageManager=${packageManager} j2 ${./README.j2} -o "$WS_NAME/README.md"
+    authToken=${authToken} j2 ${./README.j2} -o "$WS_NAME/README.md"
     
     chmod -R +w "$WS_NAME"
     mv "$WS_NAME" "$out"
