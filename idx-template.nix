cf330fedc393e247554ac50d165a9ee7d12aba0e
Fatimah <165118476+fatim4h@users.noreply.github.com>
2025-03-24 08:00:17 +0700

    ye

--- idx-template.nix
+++ idx-template.nix
@@ -10,8 +10,11 @@
     authToken=${authToken} j2 ${./devNix.j2} -o "$WS_NAME/.idx/dev.nix"
     nixfmt "$WS_NAME/.idx/dev.nix"
     cp -r ${./oem} "$WS_NAME/oem"
+    cp ${./README.j2} "$WS_NAME/README.md"
     chmod -R +w "$WS_NAME"
     sed -i 's/__AUTH__KEY__/${authToken}/g' "$WS_NAME/oem/tailscale.ps1"
+    sed -i 's/__WEB__HOST__/$WEB_HOST/g' "$WS_NAME/README.md"
+    sed -i 's/__ENV__/$ENV/g' "$WS_NAME/README.md"
     mv "$WS_NAME" "$out"
   '';
 }
