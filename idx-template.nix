6915a39e7695e04b8a985191e84e1bf7281acd94
Fatimah <165118476+fatim4h@users.noreply.github.com>
2025-03-24 08:38:55 +0700

    zz

--- idx-template.nix
+++ idx-template.nix
@@ -13,8 +13,8 @@
     cp ${./README.j2} "$WS_NAME/README.md"
     chmod -R +w "$WS_NAME"
     sed -i 's/__AUTH__KEY__/${authToken}/g' "$WS_NAME/oem/tailscale.ps1"
-    sed -i 's/__WEB__HOST__/8006-"$WEB_HOST"/g' "$WS_NAME/README.md"
-    sed -i 's/__ENV__/"$ENV"/g' "$WS_NAME/README.md"
+    sed -i "s/__WEB__HOST__/8006-$WEB_HOST/g" "$WS_NAME/README.md"
+    sed -i "s/__ENV__/$ENV/g" "$WS_NAME/README.md"
     mv "$WS_NAME" "$out"
   '';
 }
